ed56139ae6104d18fd0dfb8a9285df071bea6de4
LaurieSue <pink.bee.me@outlook.com>
2020-09-13 19:35:21 -0500

    Add project files.

---
+++ FavoriteVideo_WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function scrollAppear() {
+    var exitText = document.querySelector('.exit-text');
+    var exitPosition = exitText.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight/ 1.4; // makes it responsive to whatever size user display happens to be
+
+    if (exitPosition < screenPosition) {
+        exitText.classList.add('exit-text-appear');
+    }
+}
+window.addEventListener('scroll', scrollAppear);
+}
